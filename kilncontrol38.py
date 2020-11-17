157e7ddc7295dda33bb2ded1e5fb6a97b04d16fd
Al Anderson <skcalanderson@gmail.com>
2020-11-17 10:12:00 -0700

    Working on function for p setting callback

--- kilncontrol38.py
+++ kilncontrol38.py
@@ -413,7 +413,7 @@ class Ui_MainWindow(object):
 
     def update_p_value(self, value):
         self.pid.setKp(value/100)
-        self.p_slider_value_label.setText(str(value/100))
+        self.p_slider_value_label.setText(str(value))
 
     def manualChangeProfilePoint(self):
         global CURRENT_Temp_Profile_Number
